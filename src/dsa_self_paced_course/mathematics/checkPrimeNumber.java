33bde95946116b1b2e67f8d76d0e6e327df8fb4d
ayan878 <ayanraza197@gmail.com>
2023-12-22 23:46:43 +0530

    Check Prime Number

--- src/dsa_self_paced_course/mathematics/checkPrimeNumber.java
+++ src/dsa_self_paced_course/mathematics/checkPrimeNumber.java
@@ -9,8 +9,10 @@ public class checkPrimeNumber {
 		if(num==1) {
 			return false;
 		}
-		if(num%2==0||num%3==0) {
+		if (num==2||num==3)
 			return true;
+		if(num%2==0||num%3==0) {
+			return false;
 		}
 		for(int i=5;i*i<=num;i+=6) {
 			if(num%i==0||num%(i+2)==0) { //example 29,1031
