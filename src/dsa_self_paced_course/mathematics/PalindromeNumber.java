204fb74aaa750a075bb45be61664229443350fae
ayan878 <ayanraza197@gmail.com>
2023-12-22 23:44:06 +0530

    Palindrome Number

---
+++ src/dsa_self_paced_course/mathematics/PalindromeNumber.java
@@ -0,0 +1,34 @@
+package dsa_self_paced_course.mathematics;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    // Function to check if a number is a palindrome
+    static boolean isPalindrome(int num) {
+        int originalNumber = num;
+        int reversedNumber = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            num /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+}
