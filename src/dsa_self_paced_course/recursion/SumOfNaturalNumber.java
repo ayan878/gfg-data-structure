2cb37144cabd75f282e47a4491e4502185ad2a3f
ayan878 <ayanraza197@gmail.com>
2023-12-26 20:22:31 +0530

    Sum of Natural number

---
+++ src/dsa_self_paced_course/recursion/SumOfNaturalNumber.java
@@ -0,0 +1,18 @@
+package dsa_self_paced_course.recursion;
+
+public class SumOfNaturalNumber {
+
+	public static int SumOfNaturalNumber(int N,int k) {
+		// TODO Auto-generated constructor stub
+		if(N==1)
+			return k;
+		else 
+			return SumOfNaturalNumber(N-1, k+N);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(SumOfNaturalNumber(5, 1));
+	}
+
+}
